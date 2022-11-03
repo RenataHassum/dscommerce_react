5476f2293e5333dee2c33fbfb2757411df4d13ae
unknown <renatahassum@gmail.com>
2022-11-03 17:00:36 -0300

    Passando um objeto produto como Prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import Catalog from './routes/Catalog';
+
+import ProductDetails from './routes/ProductDetails';
 
 export default function App() {
-  return <Catalog />;
+  return <ProductDetails />;
 }
