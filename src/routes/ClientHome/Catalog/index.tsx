bff8140f19b79544e3dfb9da1193317bce24eee3
unknown <renatahassum@gmail.com>
2022-11-09 14:48:25 -0300

    Links, catalog, product details

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -2,31 +2,8 @@ import './styles.css';
 import SearchBar from '../../../components/SearchBar';
 import CatalogCard from '../../../components/CatalogCard';
 import ButtonNextPage from '../../../components/ButtonNextPage';
-import { ProductDTO } from '../../../models/product';
+import * as productService from '../../../services/product-service';
 
-const product: ProductDTO = {
-  id: 2,
-  name: 'Smart TV HD LED 32” Samsung T4300 - Wi-Fi HDR 2 HDMI 1 USB',
-  description:
-    'Curta seus filmes, série e programas favoritos com a máxima qualidade de imagem e som com a Smart TV Samsung T4300. Com uma tela LED de 32" e resolução HD, ela oferece uma quantidade muito maior de detalhes, mais nitidez e cores vibrantes que vão surpreender você e todos seus amigos e familiares',
-  imgUrl:
-    'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg',
-  price: 2500.99,
-  categories: [
-    {
-      id: 2,
-      name: 'Eletrônicos',
-    },
-    {
-      id: 3,
-      name: 'Computadores',
-    },
-    {
-      id: 4,
-      name: 'Importados',
-    },
-  ],
-};
 export default function Catalog() {
   return (
     <>
@@ -34,16 +11,9 @@ export default function Catalog() {
         <section id="catalog-section" className="dsc-container">
           <SearchBar />
           <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-            <CatalogCard product={product} />
-            <CatalogCard product={product} />
-            <CatalogCard product={product} />
-            <CatalogCard product={product} />
-            <CatalogCard product={product} />
-            <CatalogCard product={product} />
-            <CatalogCard product={product} />
-            <CatalogCard product={product} />
-            <CatalogCard product={product} />
-            <CatalogCard product={product} />
+            {productService.findAll().map((product) => (
+              <CatalogCard key={product.id} product={product} />
+            ))}
           </div>
           <ButtonNextPage />
         </section>
