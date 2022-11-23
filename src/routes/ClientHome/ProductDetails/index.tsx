988bc7fe2ad177a6659b3b4d38cd90beb980f1a7
unknown <renatahassum@gmail.com>
2022-11-23 12:55:23 -0300

    Requests for the catalog and by id

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,14 +2,25 @@ import './styles.css';
 import ButtonInverse from '../../../components/ButtonInverse';
 import ButtonPrimary from '../../../components/ButtonPrimary';
 import ProductDetailsCard from '../../../components/ProductDetailsCard';
-import * as productService from '../../../services/product-service';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { ProductDTO } from '../../../models/product';
+import axios from 'axios';
 
 export default function ProductDetails() {
-
   const params = useParams();
-  const product = productService.findById(Number(params.productId));
+  const [product, setProduct] = useState<ProductDTO>();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/products/${params.productId}`)
+      .then((response) => {
+        console.log(response);
+        setProduct(response.data);
+      });
+  }, []);
+
   return (
     <>
       <main>
@@ -19,8 +30,8 @@ export default function ProductDetails() {
           )}
           <div className="dsc-btn-page-container">
             <ButtonPrimary text="Comprar" />
-            <Link to={"/"}>
-            <ButtonInverse text="Início" />
+            <Link to={'/'}>
+              <ButtonInverse text="Início" />
             </Link>
           </div>
         </section>
