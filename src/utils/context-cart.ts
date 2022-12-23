7f09b2a6e0d60c9acc6d96b4d772031830e00c0e
unknown <renatahassum@gmail.com>
2022-12-23 18:25:22 -0300

    Global Context cart count

---
+++ src/utils/context-cart.ts
@@ -0,0 +1,16 @@
+import { createContext } from 'react';
+//CRIAR O CONTEXTO:
+// Definir o tipo do contexto (dado + função set)
+export type ContextCartCountType = {
+  contextCartCount: number;
+  setContextCartCount: (contextCartCount: number) => void;
+};
+//Criar o contexto com a função createContext
+export const ContextCartCount = createContext<ContextCartCountType>({
+  contextCartCount: 0,
+  setContextCartCount: () => {},
+});
+
+//PROVER O CONTEXTO GLOBALMENTE
+
+
