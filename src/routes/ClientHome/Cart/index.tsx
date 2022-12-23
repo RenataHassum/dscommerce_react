f7ef79d5a50deb8451974bf90d8fbad89e0af6bb
unknown <renatahassum@gmail.com>
2022-12-23 12:08:33 -0300

    Searchbar onSearch event

--- src/routes/ClientHome/Cart/index.tsx
+++ src/routes/ClientHome/Cart/index.tsx
@@ -35,54 +35,64 @@ export default function Cart() {
             </Link>
           </div>
         ) : (
-          <div className="dsc-card dsc-mb20">
-            {cart.items.map((item) => (
-              <div
-                key={item.productId}
-                className="dsc-cart-item-container dsc-line-bottom"
-              >
-                <div className="dsc-cart-item-left">
-                  <img src={item.imgUrl} alt={item.name} />
-                  <div className="dsc-cart-item-description">
-                    <h3>{item.name}</h3>
-                    <div className="dsc-cart-item-quantity-container">
-                      <div
-                        onClick={() => handleDecreaseItem(item.productId)}
-                        className="dsc-cart-item-quantity-btn"
-                      >
-                        -
-                      </div>
-                      <p>{item.quantity}</p>
-                      <div
-                        onClick={() => handleIncreaseItem(item.productId)}
-                        className="dsc-cart-item-quantity-btn"
-                      >
-                        +
+          <>
+            <div className="dsc-card dsc-mb20">
+              {cart.items.map((item) => (
+                <div
+                  key={item.productId}
+                  className="dsc-cart-item-container dsc-line-bottom"
+                >
+                  <div className="dsc-cart-item-left">
+                    <img src={item.imgUrl} alt={item.name} />
+                    <div className="dsc-cart-item-description">
+                      <h3>{item.name}</h3>
+                      <div className="dsc-cart-item-quantity-container">
+                        <div
+                          onClick={() => handleDecreaseItem(item.productId)}
+                          className="dsc-cart-item-quantity-btn"
+                        >
+                          -
+                        </div>
+                        <p>{item.quantity}</p>
+                        <div
+                          onClick={() => handleIncreaseItem(item.productId)}
+                          className="dsc-cart-item-quantity-btn"
+                        >
+                          +
+                        </div>
                       </div>
                     </div>
                   </div>
+                  <div className="dsc-cart-item-right">
+                    R$ {item.subTotal.toFixed(2)}
+                  </div>
                 </div>
-                <div className="dsc-cart-item-right">
-                  R$ {item.subTotal.toFixed(2)}
-                </div>
-              </div>
-            ))}
+              ))}
 
-            <div className="dsc-cart-total-container">
-              <h3>R$ {cart.total.toFixed(2)}</h3>
-            </div>
-            <div className="dsc-btn-page-container">
-              <div className="dsc-btn dsc-btn-blue dsc-mt20">
-                Finalizar pedido
-              </div>
-              <Link to="/catalog">
-                <div className="dsc-btn dsc-btn-white">Continuar comprando</div>
-              </Link>
-              <div onClick={handleClearClick} className="dsc-btn dsc-btn-white">
-                Limpar carrinho
+              <div className="dsc-cart-total-container">
+                <h3>R$ {cart.total.toFixed(2)}</h3>
               </div>
             </div>
-          </div>
+
+            <div>
+              <div className="dsc-btn-page-container dsc-bg">
+                <div className="dsc-btn dsc-btn-blue dsc-mt20">
+                  Finalizar pedido
+                </div>
+                <Link to="/catalog">
+                  <div className="dsc-btn dsc-btn-white">
+                    Continuar comprando
+                  </div>
+                </Link>
+                <div
+                  onClick={handleClearClick}
+                  className="dsc-btn dsc-btn-white"
+                >
+                  Limpar carrinho
+                </div>
+              </div>
+            </div>
+          </>
         )}
       </section>
     </main>
--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -5,25 +5,27 @@ import ButtonNextPage from '../../../components/ButtonNextPage';
 import { useEffect, useState } from 'react';
 import { ProductDTO } from '../../../models/product';
 import * as productService from '../../../services/product-service';
-import { CategoryDTO } from '../../../models/category';
 
 export default function Catalog() {
   const [products, setProducts] = useState<ProductDTO[]>([]);
 
+  const [productName, setProductName] = useState('');
 
   useEffect(() => {
-
-
-    productService.findAll().then((response) => {
+    productService.findPageRequest(0, productName, 12, '').then((response) => {
       setProducts(response.data.content);
     });
-  }, []);
+  }, [productName]);
+
+  function handleSearch(searchText: string) {
+    setProductName(searchText);
+  }
 
   return (
     <>
       <main>
         <section id="catalog-section" className="dsc-container">
-          <SearchBar />
+          <SearchBar onSearch={handleSearch} />
           <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
             {products.map((product) => (
               <CatalogCard key={product.id} product={product} />
