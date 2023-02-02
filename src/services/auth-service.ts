e4ae508abd80de71b257ecb60b96ef5e022d5274
unknown <renatahassum@gmail.com>
2023-02-02 12:38:14 -0300

    hasAnyRoles

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,6 +1,6 @@
 import { AxiosRequestConfig } from 'axios';
 import QueryString from 'qs';
-import { AccessTokenPayloadDTO, CredentialsDTO } from '../models/auth';
+import { AccessTokenPayloadDTO, CredentialsDTO, RoleEnum } from '../models/auth';
 import { requestBackend } from '../utils/requests';
 import { CLIENT_ID, CLIENT_SECRET } from '../utils/system';
 import * as accessTokenRepository from '../localstorage/access-token-repository';
@@ -58,3 +58,20 @@ export function isAuthenticated(): boolean {
   return false;
   // return tokenPayload && tokenPayload.exp * 1000 > Date.now() ? true : false;
 }
+
+export function hasAnyRoles(roles: RoleEnum[]): boolean {
+  if (roles.length === 0) {
+  return true;
+  }
+  const tokenPayload = getAccessTokenPayload();
+  
+  if (tokenPayload !== undefined) {
+  for (var i = 0; i < roles.length; i++) {
+  if (tokenPayload.authorities.includes(roles[i])) {
+  return true;
+  }
+  }
+  //return roles.some(role => tokenData.authorities.includes(role));
+  }
+  return false;
+  }
