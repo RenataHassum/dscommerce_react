dda5c102d0cd5a87baa555a6d0723e208bb34952
unknown <renatahassum@gmail.com>
2023-02-08 18:36:51 -0300

    Error CSS ProductForm

--- src/routes/Admin/AdminHome/ProductForm/index.tsx
+++ src/routes/Admin/AdminHome/ProductForm/index.tsx
@@ -19,7 +19,7 @@ export default function ProductForm() {
       placeholder: 'Nome',
     },
     price: {
-      value: 10,
+      value: '',
       id: 'price',
       name: 'price',
       type: 'number',
@@ -39,8 +39,6 @@ export default function ProductForm() {
   });
 
   useEffect(() => {
-    const obj = forms.validate(formData, 'price');
-    console.log(obj);
     if (isEditing) {
       productService.findById(Number(params.productId)).then((response) => {
         const newUpdateFormData = forms.updateAll(formData, response.data);
@@ -50,7 +48,13 @@ export default function ProductForm() {
   }, []);
 
   function handleInputChange(event: any) {
-    setFormData(forms.update(formData, event.target.name, event.target.value));
+    const dataUpdated = forms.update(
+      formData,
+      event.target.name,
+      event.target.value,
+    );
+    const dataValidated = forms.validate(dataUpdated, event.target.name);
+    setFormData(dataValidated);
   }
 
   return (
@@ -66,6 +70,7 @@ export default function ProductForm() {
                   onChange={handleInputChange}
                   className="dsc-form-control"
                 />
+                <div className="dsc-form-error">{formData.name.message}</div>
               </div>
               <div>
                 <FormInput
@@ -73,6 +78,7 @@ export default function ProductForm() {
                   onChange={handleInputChange}
                   className="dsc-form-control"
                 />
+                <div className="dsc-form-error">{formData.price.message}</div>
               </div>
               <div>
                 <FormInput
