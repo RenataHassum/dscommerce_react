58d3a208fb6e0fd88e749054421360c2d3c16075
unknown <renatahassum@gmail.com>
2023-02-08 17:21:51 -0300

    Forms updateAll

--- src/routes/Admin/AdminHome/ProductForm/index.tsx
+++ src/routes/Admin/AdminHome/ProductForm/index.tsx
@@ -37,7 +37,8 @@ export default function ProductForm() {
   useEffect(() => {
     if (isEditing) {
       productService.findById(Number(params.productId)).then((response) => {
-        console.log(response.data);
+        const newUpdateFormData = forms.updateAll(formData, response.data);
+        setFormData(newUpdateFormData);
       });
     }
   }, []);
